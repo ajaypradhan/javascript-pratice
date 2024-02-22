eaee7896650629254e1217e1032df2ededaae3a3
Ajay Pradhan <pradhanajay14@gmail.com>
2024-02-22 23:40:01 +0530

    variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,14 +2,19 @@ const accountId = 13243;
 let accountEmail = "test@gmail.com";
 var accountPassword = "123456";
 accountCity = "delhi";
-
+let accountState;
 // accountId = 2; //not allowed
-accountEmail = "ajay@gmail.com"
-accountPassword = "343"
-accountCity = "Noida"
+accountEmail = "ajay@gmail.com";
+accountPassword = "343";
+accountCity = "Noida";
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
-console.log(accountId)
-
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+console.log(accountId);
 
 // Prefer not to use var because of issue in block scope and functional scope
