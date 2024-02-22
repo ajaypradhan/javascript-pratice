daca60c49a39e7d2c3f1142e7fa690cc0a844aae
Ajay Pradhan <pradhanajay14@gmail.com>
2024-02-22 23:54:36 +0530

    till datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// now we don't need to add this line its automatically treat code as new version
+
+// alert(1+2) //we are using nodejs not browser
+
+let name = "Ajay"; //string
+let age = 23; //number
+let isLoggedIn = false; //boolean
+let state; //undefined
+let city = null; //null
+
+// number => 2 to power 53
+// bigint
+// string => "" || ''
+// boolean => true/false
+// undefined
+// symbol => used to find unique
+
+// object
+
+console.log(typeof "ajay"); //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
