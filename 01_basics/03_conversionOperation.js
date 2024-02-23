781069b54685b3d143d5279e7b1e363cb6fad04b
Ajay Pradhan <pradhanajay14@gmail.com>
2024-02-23 22:12:59 +0530

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+//  *******************CONVERSION*****************************
 let score = "33";
 
 console.log(typeof score);
@@ -22,3 +23,38 @@ let num = 33;
 let stringNumber = String(num);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//  *******************OPERATION*****************************
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2); //power
+console.log(2 / 2);
+console.log(2 % 2); //remainder
+
+let str1 = "hello ";
+let str2 = "ajay";
+
+let str3 = str1 + str2;
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
